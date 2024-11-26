097a17529a3b6150e4526ad9ab050504cfd3caec
dartv-in <masofi2023@gmail.com>
2024-11-26 14:33:16 +0530

    Update watch.php

--- watch.php
+++ watch.php
@@ -9,15 +9,33 @@ $playlists = [
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Check if the 'submit' button was clicked
     if (isset($_POST['submit'])) {
-        echo "<h1>Available Playlists:</h1>";
-        echo "<ul>";
+        echo "<!DOCTYPE html>
+        <html lang='en'>
+        <head>
+            <meta charset='UTF-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <title>Available Playlists</title>
+            <link href='vendor/bootstrap/css/bootstrap.min.css' rel='stylesheet'>
+        </head>
+        <body class='container mt-5'>
+            <h1 class='text-center mb-4'>Available Playlists</h1>
+            <ul class='list-group'>";
 
         // Display playlist links
         foreach ($playlists as $country => $url) {
-            echo "<li><a href=\"$url\" target=\"_blank\">$country Playlist</a></li>";
+            echo "<li class='list-group-item'>
+                    <a href='$url' target='_blank'>$country Playlist</a>
+                  </li>";
         }
 
-        echo "</ul>";
+        echo "</ul>
+            <div class='text-center mt-4'>
+                <a href='index.html' class='btn btn-primary'>Back to Home</a>
+            </div>
+            <script src='vendor/jquery/jquery.min.js'></script>
+            <script src='vendor/bootstrap/js/bootstrap.bundle.min.js'></script>
+        </body>
+        </html>";
     } else {
         echo "Invalid request: Submit button not clicked.";
     }
